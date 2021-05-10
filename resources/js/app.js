d019d64efe5f2f1e1ed99671da6ecc8c4866afd7
wargas teixeira <teixeira.wargas@gmail.com>
2021-05-10 09:19:42 -0300

    datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,20 @@
+// const modalEditEspecie = new bootstrap.Modal(document.getElementById('modal-edit-especie'))
+// const inputEspecieNome = document.getElementById('inputEspecieNome')
+// const inputEspeciePontos = document.getElementById('inputEspeciePontos')
 
-const modalEditEspecie = new bootstrap.Modal(document.getElementById('modal-edit-especie'))
-const inputEspecieNome = document.getElementById('inputEspecieNome')
-const inputEspeciePontos = document.getElementById('inputEspeciePontos')
+// VMasker(inputEspeciePontos).maskMoney()
 
-VMasker(inputEspeciePontos).maskMoney()
+// window.openEditEspecie = (especie) => {
+//   inputEspecieNome.value = especie
+//   inputEspeciePontos.value = "0,00"
+//   modalEditEspecie.show()
+// }
 
-window.openEditEspecie = (especie) => {
-  inputEspecieNome.value = especie
-  inputEspeciePontos.value = "0,00"
-  modalEditEspecie.show()
-}
+Array.from(document.querySelectorAll('.date')).forEach(elem => {
+  new Datepicker(elem, {
+    format: "dd/mm/yyyy",
+    autohide: true
+  })
+
+  console.log(elem)
+})
